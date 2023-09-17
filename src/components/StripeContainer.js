5896e70ce62bc5e5f10cc35b357fe1a5c0b7bd9c
Shrav883 <skardeka@ttu.edu>
2023-09-17 04:52:49 -0500

    Shravani 4:52am

--- src/components/StripeContainer.js
+++ src/components/StripeContainer.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Elements } from "stripe/react-stripe-js"
 import { loadStripe } from "stripe/stripe-js"
+import PaymentForm from './PaymentForm'
 
 const PUBLIC_KEY = "pk_test_51NrFOKBch0DezZegOcb1whDcFRbP8kctKQnq7CPPsLUiXLurFPmloovhECJ9fWlkUXMj4B0nfFF5iKAMOWGK7zg200f2hjgnPH"
 
